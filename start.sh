ebaa9652a888cf5543d320eafafe0ed742465c6b
Robert Norman <rnorman@tripadvisor.com>
2015-03-11 11:03:51 +0000

    incorrect path to s3cmd removed.

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ echo "access_key=$ACCESS_KEY" >> /root/.s3cfg
 echo "secret_key=$SECRET_KEY" >> /root/.s3cfg
 
 if [[ "$1" == 'no-cron' ]]; then
-    exec /usr/bin/s3cmd sync "$DATA_PATH" "$S3_PATH"
+    exec s3cmd sync "$DATA_PATH" "$S3_PATH"
 else
     echo "$CRON_SCHEDULE /usr/bin/s3cmd sync \"$DATA_PATH\" \"$S3_PATH\"" | crontab -
     exec cron -f
